5381b0b31d4b48f4b6c386939348a207770c1c35
Gollum225 <uzwpa@student.kit.edu>
2022-07-22 19:59:21 +0200

    größere Änderungen: es werden nicht mehr alle events gespechert, sondern nur noch die neuen

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,44 +1,61 @@
+import 'model/persistence.dart';
+
 /// Hält die Gesamtanzahl an Counts und die Count Historie.
 ///
 /// @author Colin Begero
 /// @version 1.0
 class Counter {
-
   static final Counter _singleton = Counter._internal();
-  late List<DateTime> _events;
+
+  // late List<DateTime> _events;
+  late List<DateTime> _newEvents;
   int _counts = 0;
+  var _database;
 
   ///Konstruktor
   factory Counter() {
     return _singleton;
   }
 
-  Counter._internal();
-
+  Counter._internal() {
+    _database = Persistence();
+    _database.init();
+    _counts = _database.getCount();
+  }
 
   add(DateTime time) {
     _counts++;
-    _events.add(time);
+    _newEvents.add(time);
     _exceptionCheck();
   }
 
   getEvents() {
-    return _events;
+    List<DateTime> allEvents = _database.getEvents;
+    allEvents.addAll(_newEvents);
+    return allEvents;
   }
+
   getCounts() {
     return _counts;
   }
 
   deleteEvent(DateTime event) {
-    _events.remove(event);
-    _counts--;
+    if (_database.getEvents().remove(event) || _newEvents.remove(event)) {
+      _counts--;
+    }
     _exceptionCheck();
   }
 
   _exceptionCheck() {
-    if (_counts != _events.length) {
+    if (_counts != _database.getEvents().length) {
       throw Exception('Unevenly incremented variables, which should be the same');
     }
+  }
+
+  //neue Ereignisse in die Datenbank schreiben und die Liste leeren.
+  writeToDatabase() {
+    _database.safe(_newEvents);
+    _newEvents = [];
   }
 
 }
