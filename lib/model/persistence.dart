b26c521a2841f025ce148585d12b2fad7a1f9f3c
Niklas Weber <niklas@webers-web.de>
2022-07-22 14:39:27 +0200

    SharedPreferences implementiert

--- lib/model/persistence.dart
+++ lib/model/persistence.dart
@@ -1,4 +1,11 @@
 import 'package:shared_preferences/shared_preferences.dart';
+
+/// Speichert Daten persistent
+/// init() Methode muss aufgerufen werden, bevor das erste Mal Daten gelesen
+/// oder geschrieben werden.
+///
+/// @author Niklas Weber
+/// @version 0.1
 class Persistence {
   static final Persistence _instance = Persistence._internal();
   late SharedPreferences _sharedPreferences;
